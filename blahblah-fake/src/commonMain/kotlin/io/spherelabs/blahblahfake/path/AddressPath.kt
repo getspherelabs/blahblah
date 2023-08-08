011ec1dc93c3bbb07198ec0796c46c17be1d6df0
Behzod Halil <behzoddev@gmail.com>
2023-08-08 17:34:58 +0900

    feat: Add address path

---
+++ blahblah-fake/src/commonMain/kotlin/io/spherelabs/blahblahfake/path/AddressPath.kt
@@ -0,0 +1,11 @@
+package io.spherelabs.blahblahfake.path
+
+enum class AddressPath(override val value: String): Path {
+    Country("address.country"),
+    State("address.state"),
+    City("address.city"),
+    StreetAddress("address.street_address"),
+    ZipCode("address.zipcode"),
+    BuildingNumber("address.building_number"),
+    StateAbbreviation("state_abbr")
+}
