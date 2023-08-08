befaf014ddc24e4034a4a5d79f6200fb3de8239e
Behzod Halil <behzoddev@gmail.com>
2023-08-08 15:12:42 +0900

    test: Add yaml provider test

---
+++ blahblah-yaml/src/commonTest/kotlin/YamlParserTest.kt
@@ -0,0 +1,36 @@
+import io.spherelabs.blahblahyaml.readPath
+import io.spherelabs.blahblahyaml.parser.yamlParser
+import okio.Path.Companion.toPath
+import kotlin.test.*
+
+
+class YamlParserTest {
+
+    @Test
+    fun `check yaml parser is working properly`() {
+        val yamlParser = yamlParser {
+            resourcePath = "src/commonTest/resources/en.yml"
+            sectionKey = "blah-blah"
+        }
+
+        val result = yamlParser.values()
+        assertNotNull(result)
+        assertEquals(1, result.values.size)
+    }
+
+    @Test
+    fun `check read resources is working properly`() {
+        val result = readPath("src/commonTest/resources/en.yml".toPath())
+        assertNotNull(result)
+        println(result)
+    }
+
+    @Test
+    fun `check yaml parser if it does not exist`() {
+        assertFailsWith(IllegalStateException::class, "Section key is not initialized") {
+            val yamlParser = yamlParser {
+                resourcePath = "src/commonTest/resources/test.yml"
+            }
+        }
+    }
+}
---
+++ blahblah-yaml/src/commonTest/kotlin/YamlProviderTest.kt
@@ -0,0 +1,21 @@
+import io.spherelabs.blahblahyaml.parser.yamlParser
+import io.spherelabs.blahblahyaml.provider.yamlProvider
+import kotlin.test.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertNotNull
+
+class YamlProviderTest {
+
+    @Test
+    fun `check provider works properly`() {
+        val provider = yamlProvider {
+            yamlParser = yamlParser {
+                resourcePath = "src/commonTest/resources/en.yml"
+                sectionKey = "blah-blah"
+            }
+        }
+
+        assertNotNull(provider.get("address.country"))
+        assertEquals('a', provider.get("address.country").last())
+    }
+}
