39ccf47575b5380b9a7857dd558fda17c482615b
behzoddev <behzoddev@gmail.com>
2023-08-10 19:16:48 +0900

    feat: Add emoji module

---
+++ blahblah-fake/src/commonMain/kotlin/io/spherelabs/blahblahfake/modules/EmojiModule.kt
@@ -0,0 +1,21 @@
+package io.spherelabs.blahblahfake.modules
+
+import io.spherelabs.blahblahfake.path.EmojiPath
+import io.spherelabs.blahblahfake.provider.Provider
+import io.spherelabs.blahblahfake.provider.ProviderType
+import io.spherelabs.blahblahfake.provider.provider
+import io.spherelabs.blahblahyaml.provider.YamlProvider
+
+class EmojiModule(
+    private val provider: Provider
+) {
+    val name: Name get() = Name(provider.get(EmojiPath.Emoji))
+}
+
+
+internal fun emojiModule(yamlProvider: () -> YamlProvider): EmojiModule {
+    return EmojiModule(provider = provider {
+        type(ProviderType.Emoji)
+        yamlProvider(yamlProvider())
+    })
+}
