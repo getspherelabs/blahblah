54dbc962f1de61724038b2eb4403432ed60ee755
behzoddev <behzoddev@gmail.com>
2023-08-10 19:18:14 +0900

    feat: Add pokemon module

---
+++ blahblah-fake/src/commonMain/kotlin/io/spherelabs/blahblahfake/modules/PokemonModule.kt
@@ -0,0 +1,31 @@
+package io.spherelabs.blahblahfake.modules
+
+import io.spherelabs.blahblahfake.path.PokemonPath
+import io.spherelabs.blahblahfake.provider.Provider
+import io.spherelabs.blahblahfake.provider.ProviderType
+import io.spherelabs.blahblahfake.provider.provider
+import io.spherelabs.blahblahyaml.provider.YamlProvider
+import kotlin.jvm.JvmInline
+
+class PokemonModule(
+    private val provider: Provider
+) {
+    val name: Name get() = Name(provider.get(PokemonPath.Name))
+    val location: Location get() = Location(provider.get(PokemonPath.Location))
+}
+
+@JvmInline
+value class Location(
+    private val value: String
+) {
+    override fun toString(): String {
+        return value
+    }
+}
+
+internal fun pokemonModule(yamlProvider: () -> YamlProvider): PokemonModule {
+    return PokemonModule(provider = provider {
+        type(ProviderType.Pokemon)
+        yamlProvider(yamlProvider())
+    })
+}
