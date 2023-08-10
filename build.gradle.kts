f7381c32f136e25927b829fb1f039d843629c70d
behzoddev <behzoddev@gmail.com>
2023-08-10 19:19:24 +0900

    chore: Update gradle and resource

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,9 @@
+buildscript {
+
+    dependencies {
+        classpath("com.android.tools.build:gradle:7.4.2")
+    }
+}
 plugins {
     //trick: for the same plugin versions in all sub-modules
     id("com.android.library").version("7.4.2").apply(false)
