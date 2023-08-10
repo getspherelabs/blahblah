589660a1577c964e69fafd45e104cd2c1cdbc97a
behzoddev <behzoddev@gmail.com>
2023-08-10 19:18:47 +0900

    feat: Add yoda module

---
+++ blahblah-fake/src/commonMain/kotlin/io/spherelabs/blahblahfake/modules/YodaModule.kt
@@ -0,0 +1,30 @@
+package io.spherelabs.blahblahfake.modules
+
+import io.spherelabs.blahblahfake.path.YodaPath
+import io.spherelabs.blahblahfake.provider.Provider
+import io.spherelabs.blahblahfake.provider.ProviderType
+import io.spherelabs.blahblahfake.provider.provider
+import io.spherelabs.blahblahyaml.provider.YamlProvider
+import kotlin.jvm.JvmInline
+
+class YodaModule(
+    private val provider: Provider
+) {
+    val quote: Quote get() = Quote(provider.get(YodaPath.Quote))
+}
+
+@JvmInline
+value class Quote(
+    private val value: String
+) {
+    override fun toString(): String {
+        return value
+    }
+}
+
+internal fun yodaModule(yamlProvider: () -> YamlProvider): YodaModule {
+    return YodaModule(provider = provider {
+        type(ProviderType.Yoda)
+        yamlProvider(yamlProvider())
+    })
+}
